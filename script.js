51d0676bcde78dabcee0672cfb12687eefb95197
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-17 13:53:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const fname = document.getElementById("fname")
+const lname = document.getElementById("lname")
+const submit = document.getElementById("submit")
+
 function getFormvalue() {
     //Write your code here
+	alert(fname.value + " " +lname.value)
 
 }
+submit.addEventListener("click",()=>{
+	getFormvalue()
+})
